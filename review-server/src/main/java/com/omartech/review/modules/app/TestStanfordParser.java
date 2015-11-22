75a8bfdb505194f588761ec8590e51f6836a2268
sonyfe25cp <sonyfe25cp@gmail.com>
2015-11-23 00:22:22 +0800

    update parser

--- review-server/src/main/java/com/omartech/review/modules/app/TestStanfordParser.java
+++ review-server/src/main/java/com/omartech/review/modules/app/TestStanfordParser.java
@@ -1,11 +1,13 @@
 package com.omartech.review.modules.app;
 
+import edu.stanford.nlp.trees.international.pennchinese.ChineseTreebankLanguagePack;
+
 /**
  * Created by OmarTech on 15-11-11.
  */
 public class TestStanfordParser {
 
     public static void main(String[] args) {
-
+        ChineseTreebankLanguagePack d;
     }
 }
