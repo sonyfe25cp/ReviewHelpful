ccbd8417591ca9aa337c8e22280c15edf8dbf8eb
Sonyfe25cp <sonyfe25cp@gmail.com>
2015-12-25 19:35:22 +0800

    update pom and create tool class

---
+++ review-server/src/main/java/com/omartech/review/server/tools/FeaturesTool.java
@@ -0,0 +1,18 @@
+package com.omartech.review.server.tools;
+
+/**
+ * Created by omar on 15/12/25.
+ */
+public class FeaturesTool {
+
+    public void tF(){
+
+    }
+
+    public void surface(){
+
+    }
+
+
+
+}
